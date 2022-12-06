3b018394d9860555001bc8fbb38f2c28c63a4011
youyyyyy <you519900911@gmail.com>
2022-12-06 14:43:11 -0500

    implement submit/update item

---
+++ global-superstore/src/main/java/com/ltp/globalsuperstore/Item.java
@@ -0,0 +1,79 @@
+package com.ltp.globalsuperstore;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Item {
+    private String id;
+    private String category;
+    private String name;
+    private Double price;
+    private Double discount;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    public Item() {
+        id = UUID.randomUUID().toString();
+    }
+
+    public Item(String category, String name, Double price, Double discount, Date date) {
+        this.category = category;
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
+}
---
+++ global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -0,0 +1,63 @@
+package com.ltp.globalsuperstore;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Controller
+public class StoreController {
+
+    private List<Item> items = new ArrayList<Item>();
+
+    @GetMapping("/")
+    public String getForm(Model model, @RequestParam(required = false) String id) {
+        int index = getItemIndex(id);
+        model.addAttribute("item", index == Constants.NOT_FOUND ? new Item() : items.get(index));
+        model.addAttribute("categories", Constants.CATEGORIES);
+        return "form";
+    }
+
+    @PostMapping("/submitItem")
+    public String handleSubmit(Item item, RedirectAttributes redirectAttributes) {
+        int index = getItemIndex(item.getId());
+        String status = Constants.SUCCESS_STATUS;
+        if (index == Constants.NOT_FOUND) {
+            items.add(item);
+        } else if (within5Days(item.getDate(), items.get(index).getDate())) {
+            items.set(index, item);
+        } else {
+            status = Constants.FAILED_STATUS;
+        }
+
+        redirectAttributes.addFlashAttribute("status", status);
+        return "redirect:/inventory";
+    }
+
+    @GetMapping("/inventory")
+    public String getInventory(Model model) {
+        model.addAttribute("items", items);
+        return "inventory";
+    }
+
+    public int getItemIndex(String id) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getId().equals(id)) {
+                return i;
+            }
+        }
+        return Constants.NOT_FOUND;
+    }
+
+    public boolean within5Days(Date newDate, Date oldDate) {
+        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
+        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
+    }
+}
