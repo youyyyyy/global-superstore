f636c70295b2808cc55d5a54a8e7dd0f2c3cf054
youyyyyy <you519900911@gmail.com>
2022-12-07 01:13:50 -0500

    implement field validation

--- global-superstore/src/main/java/com/ltp/globalsuperstore/Item.java
+++ global-superstore/src/main/java/com/ltp/globalsuperstore/Item.java
@@ -2,16 +2,27 @@ package com.ltp.globalsuperstore;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import java.util.Date;
 import java.util.UUID;
 
 public class Item {
     private String id;
+
+    @NotBlank(message = "Please choose a category")
     private String category;
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+    @Min(value = 0, message = "Price cannot be negative")
     private Double price;
+    @Min(value = 0, message = "Discount cannot be negative")
     private Double discount;
 
+    @Past(message = "Date must be in the past")
+    @NotNull(message = "Date cannot be null")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
 
@@ -74,6 +85,4 @@ public class Item {
     public void setDate(Date date) {
         this.date = date;
     }
-
-
 }
--- global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
+++ global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -2,11 +2,13 @@ package com.ltp.globalsuperstore;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -21,12 +23,17 @@ public class StoreController {
     public String getForm(Model model, @RequestParam(required = false) String id) {
         int index = getItemIndex(id);
         model.addAttribute("item", index == Constants.NOT_FOUND ? new Item() : items.get(index));
-        model.addAttribute("categories", Constants.CATEGORIES);
         return "form";
     }
 
     @PostMapping("/submitItem")
-    public String handleSubmit(Item item, RedirectAttributes redirectAttributes) {
+    public String handleSubmit(@Valid Item item, BindingResult result, RedirectAttributes redirectAttributes) {
+
+        if (item.getPrice() != null && item.getDiscount() != null && item.getPrice() <= item.getDiscount()) {
+            result.rejectValue("price", "", "Price cannot be less than the discount");
+        }
+        if (result.hasErrors()) return "form";
+
         int index = getItemIndex(item.getId());
         String status = Constants.SUCCESS_STATUS;
         if (index == Constants.NOT_FOUND) {
